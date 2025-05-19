74fab33afb31d4f739c726da6db1ea0e6de30c64
MeMeanSwp <157401283+MeMeanSwp@users.noreply.github.com>
2025-05-19 11:50:40 +0700

    Add files via upload

---
+++ src/Work/Class/Grader/Grader1ShallowCopy.java
@@ -0,0 +1,19 @@
+package Compro.Class;
+
+class Grader {
+    public String name;
+    public String desc;
+    public int pts[];
+    public String sols[];
+
+    public Grader(String name, String desc, int pts[], String sols[]) {
+        this.name = name;
+        this.desc = desc;
+        this.pts = pts;
+        this.sols = sols;
+    }
+}
+
+public class Grader1ShallowCopy {
+
+}
