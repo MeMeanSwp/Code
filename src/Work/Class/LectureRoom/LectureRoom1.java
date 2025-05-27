900a9e7f727b79a4b1f5116e272203956d0b330a
MeMeanSwp <157401283+MeMeanSwp@users.noreply.github.com>
2025-05-27 10:08:29 +0700

    Add files via upload

---
+++ src/Work/Class/LectureRoom/LectureRoom1.java
@@ -0,0 +1,18 @@
+package Compro.Class.LectureRoom;
+
+class LectRoom {
+    int classCode;
+    String name;
+    int capacity;
+    int currentSeats = 0;
+
+    LectRoom(int classCode, String name, int capacity) {
+        this.classCode = classCode;
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
+public class LectureRoom1 {
+    
+}
